11ca3120b938899becde3a7c30c1cdb2b7c05fe5
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2019-07-13 22:15:38 -0400

    Create a hard-coded todo list

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="todo-item" v-bind:class="{'completed':todo.completed}">
+    <h1>{{ todo.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+};
+</script>
+
+<style lang="scss" scoped>
+.completed {
+  color: gray;
+  text-decoration: line-through;
+}
+</style>
